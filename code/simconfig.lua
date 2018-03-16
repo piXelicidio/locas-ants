97d1efde34a4ad911187c87771d6f16bacae11c5
BESTIA\pixel <pxtracer@gmail.com>
2018-03-16 08:10:37 -0300

    sprites tweaks

--- code/simconfig.lua
+++ code/simconfig.lua
@@ -3,7 +3,7 @@
 
 local simconfig = {
   
-  numAnts = 6550,
+  numAnts = 5000,
   antMaxSpeed = 1.2,
   antComAlgorithm = 1,           -- 0 = Nothing; 1 = Pheromones inspiration  
   antComEveryFrame = false,      -- comunicate every frame? or use values of antComNeedFrameStep below  
