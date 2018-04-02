ba8f29e50dd33283087a1c5e90c2c683f0cc972b
BESTIA\pixel <pxtracer@gmail.com>
2018-04-02 01:16:51 -0300

    remove Love2D version dependency... que pase lo que pase...

--- conf.lua
+++ conf.lua
@@ -2,7 +2,6 @@
 
 function love.conf(t)
     t.identity = nil                    -- The name of the save directory (string)
-    t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
     t.console = true                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = false      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
